15a4e3e92948d33b64c087754171a0232c4b0964
jake <palatable7@naver.com>
2022-09-26 16:39:32 +0900

    Initial Commit

---
+++ ExListGridFittedGrid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExListGridFittedGrid
+//
+//  Created by 김종권 on 2022/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExListGridFittedGrid/ExListGridFittedGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExListGridFittedGridApp.swift
+//  ExListGridFittedGrid
+//
+//  Created by 김종권 on 2022/09/26.
+//
+
+import SwiftUI
+
+@main
+struct ExListGridFittedGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
